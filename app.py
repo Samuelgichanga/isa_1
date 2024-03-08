eb53d0fd4f3ec00982365483909e2b56b6fa6cd6
Samuelgichanga <133040644+Samuelgichanga@users.noreply.github.com>
2024-03-08 10:54:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 from flask import Flask, render_template, request
 from chatbot import chatbot_response
 
-app = Flask(__name__)
 app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
